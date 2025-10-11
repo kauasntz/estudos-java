39d5b988ab991cf6d5649153a6558d21b565d113
kauasntz <KauaSantos@ufrrj.br>
2025-10-11 20:18:47 -0300

    Aulas 62-63: Métodos estáticos / Blocos de inicialização estáticos

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/SerieTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Serie;
+
+public class SerieTest01 {
+    public static void main(String[] args) {
+        Serie serie1 = new Serie();
+        Serie serie2 = new Serie();
+        Serie serie3 = new Serie();
+    }
+}
