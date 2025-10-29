77199f6a0918f34a91b6f4d4343deb83fb5fac99
kauasntz <KauaSantos@ufrrj.br>
2025-10-28 22:25:26 -0300

    Aula 79: Modificador final parte 3

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -5,7 +5,12 @@ public class Carro {
     public static final double VELOCIDADE_LIMITE = 250;
     public final Comprador COMPRADOR = new Comprador();
 
-    public String getNome() {
+    public final void imprime(){
+        System.out.println(this.nome);
+    }
+
+
+     public String getNome() {
         return nome;
     }
 
