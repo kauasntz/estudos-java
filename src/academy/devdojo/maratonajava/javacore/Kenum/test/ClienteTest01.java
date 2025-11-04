44c116322e39b895ba6b2734698e1c6dedc15a24
kauasntz <KauaSantos@ufrrj.br>
2025-11-04 16:19:01 -0300

    Aula 82: Enumeração parte 3

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,14 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Liam", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
-        Cliente cliente2 = new Cliente("Noel", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente1 = new Cliente("Liam", TipoCliente.PESSOA_FISICA, TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("Noel", TipoCliente.PESSOA_JURIDICA, TipoPagamento.DEBITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
