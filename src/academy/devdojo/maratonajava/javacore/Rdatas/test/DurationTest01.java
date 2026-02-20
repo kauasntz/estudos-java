c116f6f2ee291a60ed613393d55f2abb83457215
kauasntz <KauaSantos@ufrrj.br>
2026-02-20 13:40:26 -0300

    Aula 123: Duration

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/DurationTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DurationTest01 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2).plusMinutes(5);
+        LocalTime timeNow = LocalTime.now();
+        LocalTime timeMinusSevenHours = LocalTime.now().minusHours(7);
+
+        Duration d1 = Duration.between(now, nowAfterTwoYears);
+        Duration d2 = Duration.between(timeNow, timeMinusSevenHours);
+        Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(1000));
+        Duration d4 = Duration.ofDays(20);
+
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+    }
+}
